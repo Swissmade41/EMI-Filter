0f725b7d5b50952402477fa0265714d8a1446c08
Swissmade41 <43790724+Swissmade41@users.noreply.github.com>
2019-04-15 13:12:51 +0200

    Update FiltertablePanel.java
    Tabelle angepasst

--- Software/P2Framework2019/src/team1/userinterface/FiltertablePanel.java
+++ Software/P2Framework2019/src/team1/userinterface/FiltertablePanel.java
@@ -1,5 +1,6 @@
 package team1.userinterface;
 
+import java.awt.Checkbox;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -12,35 +13,58 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.KeyStroke;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
 
+import team1.model.Model;
 import team1.util.TraceV4;
+
 
 public class FiltertablePanel extends JPanel{
 	TraceV4 trace = new TraceV4(this);
 	private Controller controller;
+	JTable table = new JTable(new MyTableModel());
+	
 
 	public FiltertablePanel(Controller controller) {
 		super(new GridLayout(1, 0));
 		trace.constructorCall();
 		
 		this.controller = controller;
-
-		JTable table = new JTable(new MyTableModel());
+		
 		table.setPreferredScrollableViewportSize(new Dimension(200, 250));
 		table.setFillsViewportHeight(true);
 
 		JScrollPane scrollPane = new JScrollPane(table);
 
-		table.getColumnModel().getColumn(1).setCellEditor(new DoubleEditor(0, 100));
+		
+		table.getColumnModel().getColumn(0).setMaxWidth(45);
+
+	//	System.out.println(table.getColumnModel().getColumnCount());
+	//	TableModel model = table.getModel();
+	//	model.addRow(new Object[]{true, "Column 3"});
+		
+		
+		//DefaultTableModel m = (DefaultTableModel) table.getModel().getRowCount()
+		//System.out.println(table.getModel().getRowCount());
+
 
 		add(scrollPane);
 	}
+	
+	public void addFilter() {
+		TableColumn aColumn = new TableColumn(1);
+//		table.getColumnModel().(new Object[]{"Column 1", "Column 2", "Column 3"});
+//		table.addColumn({{false, "Filter 1"}});
+	}
 
 	class MyTableModel extends AbstractTableModel {
-		TraceV4 trace = new TraceV4(this);
-		private String[] columnNames = { "Filtername", "Check"};
-		private Object[][] data = { { "Filter1", false,"Smith", "Snowboarding", 5}};
 
+		TraceV4 trace = new TraceV4(this);
+		private String[] columnNames = {"Enable", "Filtername" };
+		private Object[][] data = { {false, "Filter 1"},{false, "Filter 2"},{false, "Filter 3"}};
 
 		public int getColumnCount() {
 			trace.methodeCall();
@@ -58,7 +82,7 @@ public class FiltertablePanel extends JPanel{
 		}
 
 		public Object getValueAt(int row, int col) {
-			trace.methodeCall();
+			trace.methodeCall();fireTableDataChanged();
 			return data[row][col];
 		}
 
@@ -75,11 +99,12 @@ public class FiltertablePanel extends JPanel{
 
 		public boolean isCellEditable(int row, int col) {
 			trace.methodeCall();
-			if (col < 2) {
-				return false;
-			} else {
-				return true;
-			}
+//			if (col < 2) {
+//				return false;
+//			} else {
+//				return true;
+//			}
+			return true;
 		}
 
 		public void setValueAt(Object value, int row, int col) {
