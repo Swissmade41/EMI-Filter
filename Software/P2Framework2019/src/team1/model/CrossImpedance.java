2d305a54bb32c479fe6dc5b7e22462a710a6bd2a
Marco Binder <marco@haldenweg.ch>
2019-05-06 13:23:54 +0200

    Description added

--- Software/P2Framework2019/src/team1/model/CrossImpedance.java
+++ Software/P2Framework2019/src/team1/model/CrossImpedance.java
@@ -2,6 +2,9 @@ package team1.model;
 
 import org.apache.commons.math3.complex.Complex;
 
+/*
+ * takes complex Value Za and put it into a 2x2 A-type Matrix
+ */
 public class CrossImpedance {
 
 	public Complex[][] CrossImpedance(Complex Za) {
--- Software/P2Framework2019/src/team1/model/LengthImpedance.java
+++ Software/P2Framework2019/src/team1/model/LengthImpedance.java
@@ -2,6 +2,10 @@ package team1.model;
 
 import org.apache.commons.math3.complex.Complex;
 
+/*
+ * takes complex Value Zb and put it into a 2x2 A-type Matrix
+ */
+
 public class LengthImpedance {
 
 	public Complex[][] LengthImpedance(Complex Zb) {
