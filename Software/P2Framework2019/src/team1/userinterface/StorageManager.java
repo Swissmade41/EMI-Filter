e76300657b17879c6e1e5b623cfd5781ef9fb51c
Niklaus Schwegler <niklausschwegler@yahoo.de>
2019-05-06 10:05:10 +0200

    StorageManager

--- Software/P2Framework2019/src/team1/userinterface/StorageManager.java
+++ Software/P2Framework2019/src/team1/userinterface/StorageManager.java
@@ -1,5 +1,17 @@
 package team1.userinterface;
 
-public class StorageManager {
+import team1.util.TraceV4;
 
+public class StorageManager {
+	TraceV4 trace = new TraceV4(this);
+	
+	
+	/**
+	 * 
+	 */
+	public StorageManager() {
+		
+		
+		
+	}
 }
