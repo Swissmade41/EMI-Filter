36ca57f05fb2505b0909d32699d70d6bc6947e1d
Niklaus Schwegler <niklausschwegler@yahoo.de>
2019-05-06 16:09:56 +0200

    StorageManager

--- Software/P2Framework2019/src/team1/userinterface/StorageManager.java
+++ Software/P2Framework2019/src/team1/userinterface/StorageManager.java
@@ -2,16 +2,61 @@ package team1.userinterface;
 
 import team1.util.TraceV4;
 
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import javax.swing.JFileChooser;
+
 public class StorageManager {
 	TraceV4 trace = new TraceV4(this);
-	
+	private PrintWriter inputVal;
+
 	
 	/**
 	 * 
 	 */
 	public StorageManager() {
 		
-		
-		
 	}
+	public static void main(String[] args) throws Exception {  
+	//Data to write in File using PrintWriter       
+    PrintWriter writer1 =null;      
+       writer1 = new PrintWriter(new File("testout.txt"));  
+       writer1.write("Like Java, Spring, Hibernate, Android, PHP etc.");                                                   
+                       writer1.flush();  
+       writer1.close();  
+  }  
+	
+	public void Load(){
+		JFileChooser chooser = new JFileChooser();
+        chooser.showOpenDialog(null);
+        
+        int returnVal = chooser.showOpenDialog(null);
+        
+        
+        if(returnVal == JFileChooser.APPROVE_OPTION)
+        {
+            System.out.println("Die zu öffnende Datei ist: " +
+                  chooser.getSelectedFile().getName());
+        }
+    }
+	
+        
+        
+	
+	public void Save() {
+		JFileChooser chooser = new JFileChooser();
+        chooser.showSaveDialog(null);
+        
+//        if(inputVal == JFileChooser.APPROVE_OPTION)
+//        {
+//            System.out.println("Die zu speichernde Datei ist: " +
+//                  chooser.getSelectedFile().getName());
+//        }
+	}
+	
 }
