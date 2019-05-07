fc9abc36e7bc21c482bb17ac8a438e600d65a764
Lukas von Däniken <lukas.vondaeniken@icloud.com>
2019-05-07 09:25:50 +0200

    Update MikroMatlab.java
    logspace hinzugefügt

--- Software/P2Framework2019/src/team1/matlabfunctions/MikroMatlab.java
+++ Software/P2Framework2019/src/team1/matlabfunctions/MikroMatlab.java
@@ -4,56 +4,28 @@ import org.apache.commons.math3.complex.Complex;
 
 public class MikroMatlab {
 
-//	public static final double[] linspace(double begin, double end, int cnt) {
-//		double[] res = new double[cnt];
-//		double delta = (end - begin) / (cnt - 1);
-//
-//		res[0] = begin;
-//		for (int i = 1; i < res.length - 1; i++) {
-//			res[i] = begin + i * delta;
-//		}
-//		res[res.length - 1] = end;
-//
-//		return res;
-//	}
-    /**
-     * generates n logarithmically-spaced points between d1 and d2 using the
-     * provided base.
-     * 
-     * @param d1 The min value
-     * @param d2 The max value
-     * @param n The number of points to generated
-     * @param base the logarithmic base to use
-     * @return an array of lineraly space points.
-     */
-    public static final double[] logspace(double d1, double d2, int n, double base) {
-        double[] y = new double[n];
-        double[] p = linspace(d1, d2, n);
-        for(int i = 0; i < y.length - 1; i++) {
-            y[i] = Math.pow(base, p[i]);
+	/**
+	 * creates a list of logarithmic spaced values from 10^start to 10^end
+	 * @param start 
+	 * @param end
+	 * @param points number of values 
+	 * @return
+	 */
+	public double [] logspace(double d_start, double d_end, int d_points) {
+        double d_vector [] = new double[(int)d_points];
+        d_vector[0] = Math.pow(10,d_start);   //fill first cell
+        for (int i = 1; i < d_points; i++) {
+            d_vector[i] = d_vector[i-1] * Math.pow(10.0,((d_end-d_start)/(d_points-1)));
         }
-        y[y.length - 1] = Math.pow(base, d2);
-        return y;
+        return d_vector;
     }
 
     /**
-     * generates n linearly-spaced points between d1 and d2.
-     * @param d1 The min value
-     * @param d2 The max value
-     * @param n The number of points to generated
-     * @return an array of lineraly space points.
-     */
-    public static final double[] linspace(double d1, double d2, int n) {
-
-        double[] y = new double[n];
-        double dy = (d2 - d1) / (n - 1);
-        for(int i = 0; i < n; i++) {
-            y[i] = d1 + (dy * i);
-        }
-
-        return y;
-
-    }
+     * multiplies two matrices with dimensions 2*2.
+     * @param A Matrix 1 (2*2)
+     * @param B Matrix 2 (2*2)
+     * @return product of A and B
+     **/
     public Complex[][] cascade(Complex[][] A, Complex[][] B){
     	Complex[][] res = A;
     	res[0][0] = A[0][0].multiply(B[0][0]).add(A[1][0].multiply(B[0][1]));  
