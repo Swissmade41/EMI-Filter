529df1d1ef72a5d86d29e19c3e8da54f62900616
Lukas von Däniken <lukas.vondaeniken@icloud.com>
2019-05-07 13:48:28 +0200

    Update Model.java
    sollte funktionieren ;) Datensätze werden komplett gerechnet und können geholt werden

--- Software/P2Framework2019/src/team1/model/Model.java
+++ Software/P2Framework2019/src/team1/model/Model.java
@@ -9,105 +9,107 @@ import team1.matlabfunctions.MikroMatlab;
 public class Model extends Observable {
 	private TraceV4 trace = new TraceV4(this);
 	
-	private double [][][]cmData;
-	private double [][][]dmData;
 	private int s32_nbrOfCalculations = 200;
+	private double[][][] cmData = new double[100][2][s32_nbrOfCalculations];
+	private double[][][] dmData = new double[100][2][s32_nbrOfCalculations];
+	
+	private static final int L0 = 0;
+	private static final int Rp = 1;
+	private static final int Cp = 2;
+	private static final int Lr = 3;
+	private static final int Rw = 4;
+	private static final int Cx1 = 5;
+	private static final int Lx1 = 6;
+	private static final int Rx1 = 7;
+	private static final int Cx2 = 8;
+	private static final int Lx2 = 9;
+	private static final int Rx2 = 10;
+	private static final int Cy = 11;
+	private static final int Ly = 12;
+	private static final int Ry = 13;
 	
 	public Model() {
 		trace.constructorCall();
 	}
 	
-	public void readTextfile() {
-	}
-	
-	public void generateTextfile() {
-		
-	}
-	
-	public void calculate() {
-		double d_omega = 5e6*2*Math.PI;
-		
-		double d_L0 = 0.5e-3;
-		double d_Rp = 7.92e3;
-		double d_Cp = 6.46e-12;
-		double d_Rw = 11.3e-3;
-		double d_Lr = 9.2e-6;
-		
-		double d_Cx1 = 146.7e-9;
-		double d_Rx1 = 47.4e-3;
-		double d_Lx1 = 15.2e-9;
-		
-		double d_Cx2 = 146.7e-9;
-		double d_Rx2 = 47.4e-3;
-		double d_Lx2 = 15.2e-9;
-		
-		double d_Cy = 3.85e-9;
-		double d_Ry = 246e-3;
-		double d_Ly = 8e-9;
-		
+	/**
+	 * 
+	 * @param elecComponents first dimensions specifies the number of the filter 
+	 * the second dimensions store the values of the electric components
+	 */
+	public void calculate(double[][] elecComponents) {
+		double[] d_freq = MikroMatlab.logspace(0, 7, s32_nbrOfCalculations);
+		double d_omega = 6e6*2*Math.PI;
+		int s32_filter = 0;
 		// CM
-		Capacitor cm_Cy = new Capacitor(2*d_Cy, 0.5*d_Ly, 0.5*d_Ry);
-		Inductor cm_L0 = new Inductor(2*d_Cp, 0.5*d_L0, 0.5*d_Rp);
-		Inductor cm_Lr = new Inductor(0, 0.5*d_Lr, 0);
-		Resistor cm_Rw = new Resistor(0.5*d_Rw);
+		while(elecComponents[s32_filter][0] !=0) {
+			Inductor cm_L0 = new Inductor(2*elecComponents[s32_filter][Cp], 0.5*elecComponents[s32_filter][L0], 0.5*elecComponents[s32_filter][Rp]);
+			Inductor cm_Lr = new Inductor(0, 0.5*elecComponents[s32_filter][Lr], 0);
+			Resistor cm_Rw = new Resistor(0.5*elecComponents[s32_filter][Rw]);
+			Capacitor cm_Cy = new Capacitor(2*elecComponents[s32_filter][Cy], 0.5*elecComponents[s32_filter][Ly], 0.5*elecComponents[s32_filter][Ry]);
+			double d_Rb = 50;
+			Complex[][] cm_A1;
+			Complex[][] cm_A2;
+			Complex[][] cm_A;
+			Complex cm_s21;
+			double cm_IL;
+			for(int i=0; i<d_freq.length; i++) {
+				cm_A1 = MikroMatlab.getShuntImpedanceMatrix(cm_Cy.getImpedance(d_freq[i]*2*Math.PI));
+				cm_A2 = MikroMatlab.getSeriesImpedanceMatrix(cm_L0.getImpedance(d_freq[i]*2*Math.PI).add(cm_Lr.getImpedance(d_freq[i]*2*Math.PI)).add(cm_Rw.getImpedance(d_freq[i]*2*Math.PI)));			
+				cm_A = MikroMatlab.cascade(cm_A1, cm_A2);
+				cm_s21 =new Complex(2,0).divide( cm_A[0][0].add(cm_A[0][1].divide(d_Rb).add(cm_A[1][0].multiply(d_Rb)).add(cm_A[1][1])));
+				cm_IL = -20*Math.log10(cm_s21.abs());
+				System.out.println(cm_IL);
+				cmData[s32_filter][0][i] = d_freq[i];
+				cmData[s32_filter][1][i] = cm_IL;
+			}
 		
-		Complex[][] cm_A1 = getACross(cm_Cy.getImpedance(d_omega));
-		Complex[][] cm_A2 = getALength(cm_L0.getImpedance(d_omega).add(cm_Lr.getImpedance(d_omega)).add(cm_Rw.getImpedance(d_omega)));
+			// DM
+			Capacitor dm_Cy = new Capacitor(elecComponents[s32_filter][Cy], elecComponents[s32_filter][Ly], elecComponents[s32_filter][Ry]);
+			Capacitor dm_Cx1 = new Capacitor(2*elecComponents[s32_filter][Cx1], 0.5*elecComponents[s32_filter][Lx1], 0.5*elecComponents[s32_filter][Rx1]);
+			Capacitor dm_Cx2 = new Capacitor(2*elecComponents[s32_filter][Cx2], 0.5*elecComponents[s32_filter][Lx2], 0.5*elecComponents[s32_filter][Rx2]);
+			Inductor dm_Lr = new Inductor(0, elecComponents[s32_filter][Lr], 0);
+			Resistor dm_Rw = new Resistor(elecComponents[s32_filter][Rw]);
+			Complex[][] dm_A1;
+			Complex[][] dm_A2;
+			Complex[][] dm_A3;
+			Complex[][] dm_A4;
+			Complex[][] dm_A;
+			Complex dm_s21;
+			double dm_IL;
 		
-		MikroMatlab math = new MikroMatlab();
-		Complex[][] cm_A = math.cascade(cm_A1, cm_A2);
-		
-		double d_Rb = 50;
-		Complex cm_s21 =new Complex(2,0).divide( cm_A[0][0].add(cm_A[0][1].divide(d_Rb).add(cm_A[1][0].multiply(d_Rb)).add(cm_A[1][1])));
-		double cm_IL = -20*Math.log10(cm_s21.abs());
-
-		System.out.println(cm_IL);
-		
-		// DM
-		Capacitor dm_Cy = new Capacitor(d_Cy, d_Ly, d_Ry);
-		Capacitor dm_Cx1 = new Capacitor(2*d_Cx1, 0.5*d_Lx1, 0.5*d_Rx1);
-		Capacitor dm_Cx2 = new Capacitor(2*d_Cx2, 0.5*d_Lx2, 0.5*d_Rx2);
-		Inductor dm_Lr = new Inductor(0, d_Lr, 0);
-		Resistor dm_Rw = new Resistor(d_Rw);
-		
-		Complex[][] dm_A1 = getACross(dm_Cy.getImpedance(d_omega));
-		Complex[][] dm_A2 = getACross(dm_Cx1.getImpedance(d_omega));
-		Complex[][] dm_A3 = getALength(dm_Lr.getImpedance(d_omega).add(dm_Rw.getImpedance(d_omega)));
-		Complex[][] dm_A4 = getACross(dm_Cx2.getImpedance(d_omega));
-		Complex[][] dm_A = math.cascade(dm_A1, math.cascade(dm_A2, math.cascade(dm_A3, dm_A4)));
-		d_Rb = 25;
-		Complex dm_s21 =new Complex(2,0).divide( dm_A[0][0].add(dm_A[0][1].divide(d_Rb).add(dm_A[1][0].multiply(d_Rb)).add(dm_A[1][1])));
-		double dm_IL = -20*Math.log10(dm_s21.abs());
+			for(int i=0; i<d_freq.length; i++) {
+				dm_A1 = MikroMatlab.getShuntImpedanceMatrix(dm_Cy.getImpedance(d_omega));
+				dm_A2 = MikroMatlab.getShuntImpedanceMatrix(dm_Cx1.getImpedance(d_omega));
+				dm_A3 = MikroMatlab.getSeriesImpedanceMatrix(dm_Lr.getImpedance(d_omega).add(dm_Rw.getImpedance(d_omega)));
+				dm_A4 = MikroMatlab.getShuntImpedanceMatrix(dm_Cx2.getImpedance(d_omega));
+				dm_A = MikroMatlab.cascade(MikroMatlab.cascade(MikroMatlab.cascade(dm_A1, dm_A2), dm_A3), dm_A4);
+				d_Rb = 25;
+				dm_s21 =new Complex(2,0).divide( dm_A[0][0].add(dm_A[0][1].divide(d_Rb).add(dm_A[1][0].multiply(d_Rb)).add(dm_A[1][1])));
+				dm_IL = -20*Math.log10(dm_s21.abs());
+				dmData[s32_filter][0][i] = d_freq[i];
+				dmData[s32_filter][1][i] = dm_IL;
+			}
+			s32_filter++;
 		}
+		notifyObservers();
+	}
 
-	
 	public double[][][] getCM() {
 		// cmData  [Funktionsnummer],[x-Werte = 0, y-Werte = 1],[Datensätze]
-		
-		return null;
+		return cmData;
+		//return null;
 	}
 	
 	public double[][][] getDM() {
 		// dmData [Funktionsnummer],[x-Werte = 0, y-Werte = 1],[Datensätze]
-		return null;
+		return dmData;
 	}
 	
-	private Complex[][]getACross(Complex c_impedance) {
-		Complex[][] crossImp = new Complex[][] { { new Complex(1, 0), new Complex(0, 0) },
-			{ c_impedance.reciprocal(), new Complex(1, 0) } };
-		return crossImp;
-		
-	}
-	
-	private Complex[][]getALength(Complex c_impedance) {
-		Complex[][] lengthImp = new Complex[][] { { new Complex(1, 0), c_impedance},
-			{ new Complex(0, 0), new Complex(1, 0) } };
-		return lengthImp;
-	}
-//	public static void main(String args[]) {
-//		Model model = new Model();
-//		model.calculate();
-//	}
+	public void notifyObservers() {
+        setChanged();
+        super.notifyObservers();
+    }
 }
 
 
