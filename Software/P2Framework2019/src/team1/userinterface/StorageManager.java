8b85d339dee81ddcbab0993e3f9d6e41ff9c92ac
Swissmade41 <43790724+Swissmade41@users.noreply.github.com>
2019-05-08 16:26:10 +0200

    Update StorageManager.java
    save and load textfiles

--- Software/P2Framework2019/src/team1/userinterface/StorageManager.java
+++ Software/P2Framework2019/src/team1/userinterface/StorageManager.java
@@ -1,55 +1,42 @@
 package team1.userinterface;
 
-
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.swing.JFileChooser;
 
 import javax.swing.JTextArea;
 
+import org.jfree.io.IOUtils;
+
 public class StorageManager {
 
-
-	File file;
-    PrintWriter writer;
-    BufferedReader reader;
-	JTextArea anzeigeTextFeld;
-    String stringBuffer = "";
+	private File file;
+	private PrintWriter writer;
 	
-	double a[][] = {
-			{2,4,6},
-			{1,3,5}
-			};
-	
-	
-	
+	 String[] filtertable;
+	 double [][] d_effectiveParameterValues = new double[100][14];
+	 double [][] d_UserInputParameterValues = new double[100][14];
 
-	
-    public void saveFile(double [][] d_UserInputParameterValues, int [][] s32_SliderPosition) {
-        //Prepare browse dialog
-        JFileChooser jfcSave = new JFileChooser();
-       int rt = jfcSave.showSaveDialog(null);
-
-
-
-        //Show dialog
+    public void saveFile(double[][] d_UserInputParameterValues, double[][] d_effectiveParameterValues, String s_filtertable, int s32_count) {
+    	//
+    	JFileChooser jfcSave = new JFileChooser();
+    	int rt = jfcSave.showSaveDialog(null);
         if (rt == JFileChooser.APPROVE_OPTION){
-            file = jfcSave.getSelectedFile(); //Do anything u want with this file
+            file = jfcSave.getSelectedFile(); 
         }
-
-        System.out.println("write");
-
 
         try {
             writer = new PrintWriter(file.getPath()+".txt", "UTF-8");
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
         	System.err.println("File not found: ");
             e.printStackTrace();
             System.exit(1);
@@ -59,177 +46,87 @@ public class StorageManager {
             System.exit(1);
         }
         
-        writer.println("LGENDE HIER");
-        // TODO LEGENDE PARAMETER
-
-        for (int rows = 0; rows < d_UserInputParameterValues.length; rows++) {
-			stringBuffer = "";
-			for (int columns = 0; columns < d_UserInputParameterValues[0].length; columns++) {
-				if (s32_SliderPosition[rows][columns]==0) {
-					break;
-				}
-				if ((a[0].length)-1==(columns)) {
-					stringBuffer+=String.valueOf(a[rows][columns]);
-					}else  {
-						stringBuffer+=String.valueOf(a[rows][columns]) + ",";
-					} 
+        
+        
+        writer.println("Emi-Filter FHNW Gruppe1: Filtertablefile");
+        writer.println("User input parameter:");
+        //L0,RP,CP,LR,RW,CX1,LX1,Rx1,CX2,LX2,RX2,CY,LY,RY
+        for (int i = 0; i < s32_count; i++) {
+        	String  stringBuffer = "";
+			for (int j = 0; j < d_UserInputParameterValues[0].length; j++) {				
+				stringBuffer+=String.valueOf(d_UserInputParameterValues[i][j])+ ",";
 			}
-			writer.println(stringBuffer +";");
+			stringBuffer=stringBuffer.substring(0, stringBuffer.length()-1) +";";
+			writer.println(stringBuffer);
 		}
-       
-
-        // TODO SCHLEIFE Prameter
-        // Wenn null abrechen
-   
-        
-        
-        
-        writer.println("+");
-        
-        for (int rows = 0; rows < s32_SliderPosition.length; rows++) {
-			stringBuffer = "";
-			for (int columns = 0; columns < s32_SliderPosition[0].length; columns++) {
-				if ((s32_SliderPosition[0].length)-1==(columns)) {
-					stringBuffer+=String.valueOf(s32_SliderPosition[rows][columns]);
-					}else  {
-						stringBuffer+=String.valueOf(s32_SliderPosition[rows][columns]) + ",";
-					} 
+           
+        writer.println("");
+        writer.println("Effective parameter value:"); 
+        for (int i = 0; i < s32_count; i++) {
+        	String  stringBuffer = "";
+			for (int j = 0; j < d_UserInputParameterValues[0].length; j++) {				
+				stringBuffer+=String.valueOf(d_effectiveParameterValues[i][j])+ ",";
 			}
-		
+			stringBuffer=stringBuffer.substring(0, stringBuffer.length()-1) +";";
+			writer.println(stringBuffer);
 		}
+        
+        writer.println("");
+        writer.println("Filtertable:");
+        writer.println(s_filtertable); 
         
         writer.close();
-
     }
     
-    public void loadFile() throws UnsupportedEncodingException {
-    	
-    	anzeigeTextFeld = new JTextArea(5, 20);
-//    	JScrollPane scrollPane = new JScrollPane(anzeigeTextFeld); 
-    	anzeigeTextFeld.setEditable(false);
-
-    	
-        //Prepare browse dialog
+    public void loadFile() throws UnsupportedEncodingException { 	
         JFileChooser jfcLoad = new JFileChooser();
-       int rt = jfcLoad.showOpenDialog(null);
-//        fc.setFileHidingEnabled(false);
-//        fc.setAcceptAllFileFilterUsed(false);
-//        fc.setApproveButtonText("Open");
-
-
-        //Show dialog
-//        int rt = jfcSave.showOpenDialog(null); //someframe is  JFrame
+        int rt = jfcLoad.showOpenDialog(null);
         if (rt == JFileChooser.APPROVE_OPTION){
-            file = jfcLoad.getSelectedFile(); //Do anything u want with this file
+            file = jfcLoad.getSelectedFile(); 
         }
 
-        System.out.println("reade");
-
-
+        String stringBuffer = ""; 
         try {
-            reader = new BufferedReader(new FileReader(file));
-            anzeigeTextFeld.setText("");
-            String zeile;
-            while ((zeile = reader.readLine()) != null){
-                anzeigeTextFeld.append(zeile + "\n");
-                
-            }
-            reader.close();
-        } catch (IOException exe) {
-            // TODO Auto-generated catch block
-            System.err.println("Datenfehler: ");
-         // TODO Datenfehlerausgeben
-            System.exit(1);
-        }
+        	stringBuffer = new String(Files.readAllBytes(Paths.get(file.getPath())));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
+        String[] splitfile = stringBuffer.replaceAll("Effective parameter value","").replaceAll("Filtertable","").replace("\n", "").replace("\r", "").split(":");
         
-  
+        //Separate user input parameter
+        String[] userinput = splitfile[2].split(";");   
+        for (int i = 0; i < userinput.length; i++) {
+        	String[] userInputLines = userinput[i].split(",");
+        	for (int j = 0; j < userInputLines.length; j++) {
+				d_UserInputParameterValues[i][j]=Double.parseDouble(userInputLines[j]);			
+			}
+		}
         
-
+        //Separate slider position
+        String[] effectiveValue = splitfile[3].split(";");         
+        for (int i = 0; i < effectiveValue.length; i++) {
+        	String[] sliderPositionLine = effectiveValue[i].split(",");
+        	for (int j = 0; j < sliderPositionLine.length; j++) {
+        		d_effectiveParameterValues[i][j]=Double.parseDouble(sliderPositionLine[j]);			
+			}
+		}
+        
+        //Seperate filtertable
+        filtertable = splitfile[4].replace(";", "").split(",");
     }
-	
-    public static void main(String args[]) throws IOException {
-    	StorageManager storageManager = new StorageManager();
-    	storageManager.loadFile();
-	}
-   
-	
-//	TraceV4 trace = new TraceV4(this);
-//	private final JFileChooser jfcLoad = new JFileChooser(new File(".//"));
-//	private final JFileChooser jfcSave = new JFileChooser(new File(".//"));
-//	
-//	private static PrintWriter ausgabeDatei;
-//	
-//	
-//	/**
-//	 * 
-//	 */
-//	public StorageManager() {
-//	
-//	}
-//	
-//	public static void print(String[] textWrite) throws Exception {
-//		PrintWriter ausgabeDatei =null;      
-//	       ausgabeDatei = new PrintWriter(new File("testout.txt"));  
-//	       ausgabeDatei.write("HALLO12");                                                   
-//	       		ausgabeDatei.flush();  
-//	       		ausgabeDatei.close();  
-//	}
-//	
-//	public void Load(){
-//	JFileChooser chooser = new JFileChooser();
-//    chooser.showOpenDialog(null);
-//    
-//    int returnVal = chooser.showOpenDialog(null);
-//    
-//    
-//    if(returnVal == JFileChooser.APPROVE_OPTION)
-//    {
-//        System.out.println("Die zu öffnende Datei ist: " +
-//              chooser.getSelectedFile().getName());
-//    }
-//}
-
     
+    public double[][] getUserInputParameterValues() {
+    	return d_UserInputParameterValues;
+    }
     
-
-//	public void Save() {
-//		JFileChooser chooser = new JFileChooser();
-//		chooser.showSaveDialog(null);
-//    
-//		if(inputVal == JFileChooser.APPROVE_OPTION){
-//			System.out.println("Die zu speichernde Datei ist: " +
-//              chooser.getSelectedFile().getName());
-//		}
-//	}
-
-
-	
-//	public void actionPerformed(ActionEvent e) {
-//		if(e.getActionCommand().equals("Load filter profile")){
-//			System.out.println("hallo");
-//			int returnVal = jfcLoad.showOpenDialog(this);
-//			if(returnVal == JFileChooser.APPROVE_OPTION){
-//				File file = jfcLoad.getSelectedFile();
-//				System.out.println(""+file.isDirectory());
-//				System.out.println(""+file.getName());
-//				System.out.println(""+file.getAbsolutePath());
-//				System.out.println(textRead(file.getAbsolutePath())[0]);
-//			}
-//		}
-//		if(e.getActionCommand().equals("Save filter profile")){
-//			int returnVal = jfcSave.showSaveDialog(this);
-//			if(returnVal == JFileChooser.APPROVE_OPTION){
-//				File file = jfcLoad.getSelectedFile();
-//				System.out.println(""+file.isDirectory());
-//				System.out.println(""+file.getName());
-//				System.out.println(""+file.getAbsolutePath());
-//				System.out.println(textRead(file.getAbsolutePath())[0]);
-//			}
-//		}
-
-//	}
-
-	
+    public double[][] getEffectiveParameterValues() {
+    	return d_effectiveParameterValues;
+    }
+    
+    public String[] getFiltertable() {
+    	return filtertable;
+    }
 }
 
